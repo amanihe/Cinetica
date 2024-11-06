63b7e9d8911a07d505cc8dc5cb9ff14acb030387
Amani.HEDIA <amani.hedia@holydis.com>
2024-11-06 12:50:28 +0100

    init api project fix

--- app/api/shows/top-rated/route.ts
+++ app/api/shows/top-rated/route.ts
@@ -1,26 +1,33 @@
 import { TVShow } from '@/app/entities/TVShow';
 import { NextResponse } from 'next/server';
 
+
 export async function GET() {
-  const res = await fetch(`https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.TMDB_API_KEY}`);
-  const data = await res.json();
+    console.log(process.env.TMDB_API_KEY);
+    const res = await fetch(`https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.TMDB_API_KEY}`);
+    
+    if (!res.ok) {
+        return NextResponse.json({ error: 'Failed to fetch data' }, { status: res.status });
+    }
 
-  const series: TVShow[] = data.results.map((show: TVShow) => ({
-    adult: show.adult,
-    chemin_fond: show.backdrop_path,
-    genres_ids: show.genre_ids,
-    id: show.id,
-    pays_origine: show.origin_country,
-    langue_originale: show.original_language,
-    nom_original: show.original_name,
-    description: show.overview,
-    popularite: show.popularity,
-    chemin_affiche: show.poster_path,
-    date_premiere: show.first_air_date,
-    nom: show.name,
-    moyenne_votes: show.vote_average,
-    nombre_votes: show.vote_count,
-  }));
+    const data = await res.json();
 
-  return NextResponse.json(series);
+    const shows: TVShow[] = data.results.map((show: TVShow) => ({
+        adult: show.adult,
+        backdrop_path: show.backdrop_path,
+        genre_ids: show.genre_ids,
+        id: show.id,
+        origin_country: show.origin_country,
+        original_language: show.original_language,
+        original_name: show.original_name,
+        overview: show.overview,
+        popularity: show.popularity,
+        poster_path: show.poster_path,
+        first_air_date: show.first_air_date,
+        name: show.name,
+        vote_average: show.vote_average,
+        vote_count: show.vote_count,
+    }));
+
+    return NextResponse.json(shows);
 }
